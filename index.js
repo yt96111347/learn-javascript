88e3fd9ac26424ed9b9cbd7ab779dfdaec2c4730
yt96111347 <yt96111347@gmail.com>
2026-09-15 10:52:18 +0900

    변수 선언 및 확인

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 console.log("hello js")
 
 // 변수: 값을 저장하는 이름을 가진 공간
-const username = "태리";
+let username = "태리";
+console.log("🚀 ~ username:", username)
+
+username = "테스트"
 
 console.log("username", username)
+
+// 데이터 타입
+const num = 10 //숫자
+console.log("num", num, "num type", typeof num)
+
+const text = " 안녕 ~ "
+console.log("🚀 ~ text:", text)
+
+var isDarkMode = true //불리언
+console.log("🚀 ~ isDarkMode:", isDarkMode, typeof isDarkMode)
+
+var empty = null
+console.log("🚀 ~ empty:", empty, typeof empty)
+
+
+

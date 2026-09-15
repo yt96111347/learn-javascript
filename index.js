7ed42f6c3075f762245aeae2938a8a8fa8f13018
yt96111347 <yt96111347@gmail.com>
2026-09-15 13:51:31 +0900

    함수 실습

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ console.log("hello js")
 let username = "태리";
 console.log("🚀 ~ username:", username)
 
-username = "테스트"
+//username = "테스트"
 
 console.log("username", username)
 
@@ -20,6 +20,50 @@ console.log("🚀 ~ isDarkMode:", isDarkMode, typeof isDarkMode)
 
 var empty = null
 console.log("🚀 ~ empty:", empty, typeof empty)
+/**
+ * 함수: 특정 기능을 수행하도록 만든 코드 조각
+ */
 
+// function greeting (){
+//   console.log(`${username} 님 반갑습니다.`)
+//   // console.log(username, "님 반갑습니다.")
+// }
 
+// 화살표로 함수 선언 하는 걸로 변경됨
+const greeting = () => {
+  console.log(`${username} 님 반갑습니다.~~`)
+  // console.log(username, "님 반갑습니다.")
+}
+
+//함수 실행
+greeting()
+
+// 미션 : "AI챗봇 수강생 태리 입니다. " 함수 만들기
+const Hello =()=>{
+  console.log(`AI챗봇 수강생 ${username} 입니다.`)
+}
+
+Hello()
+// 파라메타
+const greeting_f = (studname, usermbti) => {
+  console.log(`${studname} 님 반갑습니다.~~`)
+  console.log(` MBTI는 ${usermbti} 이시군요.~~`)
+  // console.log(username, "님 반갑습니다.")
+}
+greeting_f()
+greeting_f("태술리", "ISTJ")
+
+//결과값 반환
+const add =(num1, num2) => {
+  return num1+num2
+}
+
+const sum = add(10,20)
+console.log("🚀 ~ sum:", sum)
+
+const doublesum = (num) =>{
+  return num * 2
+}
+
+console.log("doublesum:", doublesum(sum))
 
